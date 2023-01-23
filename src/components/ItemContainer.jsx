6cd612c152363b89273e311590a40157c3cc501a
Marcy Villegas <marcyvillegas17@gmail.com>
2023-01-23 16:17:21 +0800

    Added implemenation of mapDispatchToProps with a second parameter

--- src/components/ItemContainer.jsx
+++ src/components/ItemContainer.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { buyCake, buyIceCream } from '../redux';
 
 function ItemContainer(props) {
     return (
         <div>
-            <h1>Component with a prop itself {"(with a condition statement)"} using mapStateToProps</h1>
+            <h1>Component with a prop itself {"(with a condition statement)"} using mapStateToProps and mapDispatchToProps</h1>
             <h2>Item is {props.nameOfItem} {props.item}</h2>
+            <button onClick={props.buyItem}>Buy {props.nameOfItem}</button>
         </div>
     );
 }
@@ -22,4 +24,16 @@ const mapStateToProps = (state, ownProps) => {
     }
 }
 
-export default connect(mapStateToProps)(ItemContainer);
+const mapDispatchToProps = (dispatch, ownProps) => {
+
+    const dispatchFunction = ownProps.cake
+        ? () => dispatch(buyCake())
+        : () => dispatch(buyIceCream())
+
+    return {
+        buyItem: dispatchFunction
+    }
+}
+
+// can replace [name]ToProps function with null if one will not be used
+export default connect(mapStateToProps, mapDispatchToProps)(ItemContainer);
